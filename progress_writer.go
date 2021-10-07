b78e9d7cf9db6f79a4fe9f8d5011e6f7246c130e
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-10-06 21:26:21 -0700

    Adding TotalProgressWriter interface suitable for dolo (and similar kind of applications)

--- progress_writer.go
+++ progress_writer.go
@@ -1,18 +1,36 @@
 package nod
 
-type writer struct {
+import "io"
+
+type TotalProgressWriter interface {
+	io.Writer
+	Progress(uint64)
+	Total(uint64)
+}
+
+type totalProgress struct {
 	topics  []string
+	total   uint64
 	current uint64
 }
 
-func (w *writer) Write(bytes []byte) (int, error) {
-	w.current = w.current + uint64(len(bytes))
-	dispatch(MsgProgress, w.current, w.topics...)
+func (tp *totalProgress) Write(bytes []byte) (int, error) {
+	tp.Progress(tp.current + uint64(len(bytes)))
 	return len(bytes), nil
 }
 
-func ProgressWriter(topics ...string) *writer {
-	return &writer{
+func (tp *totalProgress) Progress(current uint64) {
+	tp.current = current
+	dispatch(MsgProgress, tp.current, tp.topics...)
+}
+
+func (tp *totalProgress) Total(total uint64) {
+	tp.total = total
+	dispatch(MsgTotal, tp.total, tp.topics...)
+}
+
+func TotalProgress(topics ...string) *totalProgress {
+	return &totalProgress{
 		topics: topics,
 	}
 }
