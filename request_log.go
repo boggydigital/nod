e3936abc3ae3f518df8d94dbc79fc4ea10a07ae7
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2022-03-14 16:04:31 -0700

    Adding comment

--- request_log.go
+++ request_log.go
@@ -2,6 +2,7 @@ package nod
 
 import "net/http"
 
+//RequestLog is a trivial logging middleware that logs the URL of the request
 func RequestLog(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		Log("%s %s", r.Method, r.URL)
